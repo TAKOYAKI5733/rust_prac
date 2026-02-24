c8487dfe2c78a6a2088aef59b83ebd9641c4c2cd
TAKOYAKI5733 <hosoyachiharuka@gmail.com>
2026-02-25 01:25:19 +0900

    backend test

---
+++ main/backend/server.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from "express";
+import fs, { read, write } from "fs";
+import path from "path";
+import cors from "cors";
+import { resolve } from "dns";
+import { error } from "console";
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+const DATA_FILE = path.join(__dirname, "DB.json");
+
+const readData = (): Item[] => {
+  if (!fs.existsSync(DATA_FILE)) return [];
+  const json = fs.readFileSync(DATA_FILE, "utf-8");
+  return JSON.parse(json) as Item[];
+};
+
+const writeData = (data: Item[]) => {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/items", (req: Request, res: Response) => {
+  const data = readData();
+  res.json(data);
+});
+
+app.post("/items", (req: Request, res: Response) => {
+  const data = readData();
+  const newItem: Item = req.body;
+  data.push(newItem);
+  writeData(data);
+  res.json({ status: "ok", item: newItem });
+});
+
+app.put("/items/:id", (req: Request<{ id: string }>, res: Response) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid id" });
+  const data = readData();
+
+  const index = data.findIndex(item => item.id === id);
+  if (index === -1) return res.status(404).json({ error: "Not found" });
+  data[index] = { ...data[index], ...req.body };
+  writeData(data);
+  res.json({ status: "ok", item: data[index] });
+});
+
+app.delete("/items/:id", (req: Request<{ id: string }>, res: Response) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid id" });
+
+  let data = readData();
+  const exists = data.some(item => item.id === id);
+  if (!exists) return res.status(404).json({ error: "Not found" });
+  data = data.filter(item => item.id !== id);
+  writeData(data);
+  res.json({ status: "ok" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
